3264471c5f4b2983b297fc25951818b2b64ba826
Blvckiiing <mbithimusa@gmail.com>
2023-03-17 22:50:46 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /* This program prints a message using the `puts` function.*/
 int main(void)
 {
 	/* Print the message. */
 	puts("Programming is like building a multilingual puzzle");
-	
 	/* Return 0 to indicate successful execution. */
 	return (0);
 }
