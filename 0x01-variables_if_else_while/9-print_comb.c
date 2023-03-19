8d76a7584db0d305ae05be86abb01fcb52391a61
Blvckiiing <mbithimusa@gmail.com>
2023-03-19 10:04:44 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible different combinations of two digits.
+ * main - Prints all possible combinations of single-digit numbers.
  *
- * Description: Numbers must be separated by ', ', and the two digits must be different.
- * * 01 and 10 are considered the same combination of the two digits 0 and 1.
- * Only the smallest combination of two digits should be printed.
- * Numbers should be printed in ascending order, with two digits.
- *
- * Return: 0 on successful completion.
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, j;
+	int num;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num < 10; num++)
 	{
-		for (j = i + 1; j < 10; j++)
+		putchar(num + '0');
+		if (num < 9)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i == 8 && j == 9)
-				continue;
 			putchar(',');
 			putchar(' ');
 		}
