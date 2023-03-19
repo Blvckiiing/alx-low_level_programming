aac61cc3f8d6ebd9abe5b94e86c269962f33eede
Blvckiiing <mbithimusa@gmail.com>
2023-03-19 09:31:43 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
-/**
- * main - Entry point
+/*
+ * Prints all possible two-digit combinations of the digits 0-9.
  *
- * Return: Always 0 (Success)
+ * This program prints all possible two-digit combinations of the digits 0-9,
+ * separated by commas and spaces. The digits are printed in ascending order.
+ *
+ * Returns:
+ *     0 on successful completion.
  */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for (j = i + 1; j <= 9; j++)
+		for (j = i + 1; j < 10; j++)
 		{
 			putchar(i + '0');
-			putchar(',');
-			putchar(' ');
 			putchar(j + '0');
-			if (i == 8 && j == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (i < 8 || j < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
