e6bc51dc9d0a26add2abbb2ec41e70501b246300
Blvckiiing <mbithimusa@gmail.com>
2023-07-07 06:16:51 -0700

    Retrying connection

---
+++ 0x09-static_libraries/my_functions.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+int _putchar(char c) {
+    // Implementation of _putchar
+}
+
+int _islower(int c) {
+    // Implementation of _islower
+}
+
+int _isalpha(int c) {
+    // Implementation of _isalpha
+}
+
+// ... Implement the remaining functions similarly
