bdc466a545fadff4462958dc8a20867188986120
Blvckiiing <mbithimusa@gmail.com>
2023-09-29 15:52:28 -0700

    Updates on the bit manipulation project

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - Converts a binary string to an unsigned int.
+ * @b: The binary string to convert.
+ * Return: The converted unsigned int or 0 if an error occurs.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
+	unsigned int decimal_value = 0;
+
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		decimal_value = decimal_value * 2 + (b[i] - '0');
+	}
+
+	return (decimal_value);
+}
---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to standard output.
+ * @c: The character to be printed.
+ *
+ * Return: On success, returns 1.
+ * On error, returns -1 and sets errno appropriately.
+ */
+
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
