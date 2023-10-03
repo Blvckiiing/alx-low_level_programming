863819f7775e5427e64bdb1df23c2d478f05dc9b
Blvckiiing <mbithimusa@gmail.com>
2023-10-03 13:07:26 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 /**
  * create_file - Creates a file and writes text content to it.
@@ -19,26 +20,31 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-	{
 		for (len = 0; text_content[len]; len++)
 			;
-	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
+}
+
+fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+if (fd == -1)
+	return (-1);
+if (text_content != NULL)
+{
+	w = write(fd, text_content, len);
+
+	if (w == -1)
 	{
-		w = write(fd, text_content, len);
-
-		if (w == -1)
-			;
-		{
-			close(fd);
-
-			return (-1);
-		}
+		close(fd);
+		return (-1);
 	}
-	close(fd);
+}
 
-	return (1);
+if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
+{
+	close(fd);
+	return (-1);
+}
+
+close(fd);
+
+return (1);
 }
