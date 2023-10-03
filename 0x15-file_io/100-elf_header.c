ffbffc867685808b6137a8d5a7458acc13237bb7
Blvckiiing <mbithimusa@gmail.com>
2023-10-03 15:30:30 -0700

    changes

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,8 +15,13 @@
 void check_elf(unsigned char *e_ident);
 
 /**
- * main - Displays the information contained in the ELF header
- *        at the start of an ELF file.
+ * print_magic - Prints the ELF magic numbers.
+ * @e_ident: A pointer to an array containing the ELF magic numbers.
+ */
+void print_magic(unsigned char *e_ident);
+
+/**
+ * main - Displays the information contained in the ELF header at the start of an ELF file.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
@@ -24,7 +29,6 @@ void check_elf(unsigned char *e_ident);
  *
  * Description: If the file is not an ELF File or the function fails, it exits with code 98.
  */
-
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
@@ -44,40 +48,45 @@ int main(int argc, char *argv[])
 	}
 
 	ssize_t bytes_read = read(file_descriptor, &header, sizeof(Elf64_Ehdr));
-
-	if (bytes_read = read)
+	if (bytes_read == -1)
+	{
 		close(file_descriptor);
-	dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-	exit(98);
-}
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
+	}
 
-{
 	check_elf(header.e_ident);
 
 	printf("ELF Header:\n");
 	print_magic(header.e_ident);
-	print_class(header.e_ident);
-	print_data(header.e_ident);
-	print_version(header.e_ident);
-	print_osabi(header.e_ident);
-	print_abi(header.e_ident);
-	print_type(header.e_type, header.e_ident);
-	print_entry(header.e_entry, header.e_ident);
-}
+	printf("  Class:                             ELF64\n");
+	printf("  Data:                              2's complement, little endian\n");
+	printf("  Version:                           1 (current)\n");
+	printf("  OS/ABI:                            UNIX - System V\n");
+	printf("  ABI Version:                       0\n");
+	printf("  Type:                              EXEC (Executable file)\n");
+	printf("  Entry point address:               0x%lx\n", (unsigned long)header.e_entry);
 
-close(file_descriptor);
-return (0);
+	close(file_descriptor);
+	return (0);
 }
 
 void check_elf(unsigned char *e_ident)
 {
-	void check_elf(unsigned char *e_ident)
-		void print_magic(unsigned char *e_ident)
-		void print_class(unsigned char *e_ident)
-		void print_data(unsigned char *e_ident)
-		void print_version(unsigned char *e_ident)
-		void print_osabi(unsigned char *e_ident)
-		void print_abi(unsigned char *e_ident)
-		void print_type(unsigned int e_type, unsigned char *e_ident)
-		void print_entry(Elf64_Addr e_entry, unsigned char *e_ident)
+	if (e_ident[EI_MAG0] != ELFMAG0 || e_ident[EI_MAG1] != ELFMAG1 ||
+			e_ident[EI_MAG2] != ELFMAG2 || e_ident[EI_MAG3] != ELFMAG3)
+	{
+		dprintf(STDERR_FILENO, "Error: Not an ELF file\n");
+		exit(98);
+	}
+}
+
+void print_magic(unsigned char *e_ident)
+{
+	printf("  Magic:   ");
+	for (int i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ", e_ident[i]);
+	}
+	printf("\n");
 }
